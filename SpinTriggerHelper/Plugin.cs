9cdbf626e37a385acd36e1d2fae070f75e2c568f
Raoul1808 <raoulthegeek@gmail.com>
2024-04-24 23:59:02 +0200

    Bump up version

--- SpinTriggerHelper/Plugin.cs
+++ SpinTriggerHelper/Plugin.cs
@@ -9,7 +9,7 @@ namespace SpinTriggerHelper
     {
         private const string Guid = "srxd.raoul1808.triggerhelper";
         private const string Name = "Spin Trigger Helper";
-        private const string Version = "0.1.0";
+        private const string Version = "1.0.0";
         
         private static ManualLogSource _logger;
         
