9ef1b1506975b65ea9fc25cd5e10aa8fd35c3e50
Raoul1808 <raoulthegeek@gmail.com>
2024-04-28 00:01:07 +0200

    Optimize trigger lookup and update event calls

--- SpinTriggerHelper/ModTriggerStore.cs
+++ SpinTriggerHelper/ModTriggerStore.cs
@@ -52,19 +52,38 @@ namespace SpinTriggerHelper
         {
             // If the current trigger is out of time and the previous trigger is set, this means there was some sort of rewind effect
             // Rewinding shouldn't happen when actually playing the game, so while this might cause some calculation and indexing overhead, I consider this fine enough to not throw players off.
-            if (_currentTrigger?.Time > trackTime && _previousTrigger != null)
-                Reset();
-            while (trackTime >= _nextTrigger?.Time)
+            if (_previousTrigger != null && trackTime < _currentTrigger?.Time)
             {
-                OnTriggerUpdate?.Invoke(_currentTrigger, _nextTrigger.Time);
-                _previousTrigger = _currentTrigger;
-                _currentTrigger = _nextTrigger;
-                _nextTrigger = null;
-                _currentOrNextIndex++;
-                if (_triggers.Count > _currentOrNextIndex)
+                while (_previousTrigger != null && trackTime < _currentTrigger?.Time)
                 {
-                    _nextTrigger = _triggers[_currentOrNextIndex];
+                    _nextTrigger = _currentTrigger;
+                    _currentTrigger = _previousTrigger;
+                    _previousTrigger = null;
+                    _currentOrNextIndex--;
+                    if (_currentOrNextIndex > 0)
+                    {
+                        _previousTrigger = _triggers[_currentOrNextIndex];
+                    }
                 }
+            }
+            // if (_currentTrigger?.Time > trackTime && _previousTrigger != null)
+            //     Reset();
+            if (trackTime >= _nextTrigger?.Time)
+            {
+                while (trackTime >= _nextTrigger?.Time)
+                {
+                    _previousTrigger = _currentTrigger;
+                    _currentTrigger = _nextTrigger;
+                    _nextTrigger = null;
+                    _currentOrNextIndex++;
+                    if (_triggers.Count > _currentOrNextIndex)
+                    {
+                        _nextTrigger = _triggers[_currentOrNextIndex];
+                    }
+                }
+                
+                if (_nextTrigger != null)
+                    OnTriggerUpdate?.Invoke(_currentTrigger, _nextTrigger.Time);
             }
             if (trackTime >= _currentTrigger?.Time)
             {
